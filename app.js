5b9a2501976198efad9864368a35d900e95f12cb
Rahul Kumar <rahul0809@outlokk.com>
2019-08-20 18:44:11 +0530

    signup module with web token login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
+const passport = require('passport');
 
 // middleware bodyparser
 app.use(bodyparser.urlencoded({ extended: false }));
@@ -9,15 +10,21 @@ app.use(bodyparser.json());
 const auth = require('./routes/api/auth');
 const notes = require('./routes/api/notes');
 
-// initilize the database variable
+// initialize the database variable
 const db = require('./setup/myurl').mongoURL;
 
 // connecting to database
 mongoose.set('useNewUrlParser', true);
 mongoose
   .connect(db)
-  .then(console.log('mongoDb connected sucessfully......'))
+  .then(console.log('mongoDb connected successfully......'))
   .catch(err => console.log('err from mongodb connection' + err));
+
+// initialize the passport middleware
+app.use(passport.initialize());
+
+// config for JWt Strategies
+require('./strategies/jsonwtStrategies')(passport);
 //testing
 app.get('/', (req, res) => {
   res.json({ msg: 'testing of expressJs' });
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-
+const jsonwt = require('jsonwebtoken');
+const passport = require('passport');
+const key = require('../../setup/myurl');
 const User = require('../../models/user');
 
 // testing purpose
@@ -33,22 +35,79 @@ router.post('/signup', (req, res) => {
         });
         // encrypt the password
         bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(req.body.password, salt, (err, hash) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
             // Store hash in your password DB.
             newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log('error from user saving' + err));
           });
         });
-        newUser
-          .save()
-          .then(newUser => res.json(newUser))
-          .catch(err =>
-            console.log('err from signup router at saving method' + err)
-          );
       }
     })
     .catch(err =>
-      console.log('err from signup route from findone method' + err)
+      console.log('err from signup route from findOne method' + err)
     );
 });
+
+// @type => POST
+// @route => api/auth/login
+// @desc => route for login
+// @access => PUBLIC
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res
+          .status(404)
+          .json({ userErr: 'user not found. Check your email and try again' });
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(isCorrect => {
+          if (!isCorrect) {
+            return res.json({ passwordError: 'password is not match' });
+          }
+          // console.log(user.id, user.name, user.email, user.username);
+          const payload = {
+            id: user.id,
+            name: user.name,
+            email: user.email
+          };
+          // console.log(payload);
+          jsonwt.sign(
+            payload,
+            key.secret,
+            { expiresIn: 3600 },
+            (err, token) => {
+              res.json({
+                success: true,
+                //  token: 'Bearer ' + token
+                token: token
+              });
+            }
+          );
+        })
+        .catch(err =>
+          console.log('err from login route from bcrypt compare method' + err)
+        );
+    })
+    .catch(err => console.log('error from login route' + err));
+});
+
+//testing
+router.get(
+  '/profile',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    // console.log(req);
+    res.json(req.user);
+  }
+);
 
 module.exports = router;
