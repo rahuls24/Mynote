400d3fe17b6c46c36dde50cfa4f578b0d7378d21
Rahul Kumar <rahul080924@outlook.com>
2019-08-24 18:50:51 +0530

    enable cross
    enable cross that will help to use post method from same function

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const passport = require('passport');
 // middleware bodyparser
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
+
+// enable cross that will help to send data from same system 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const auth = require('./routes/api/auth');
 const notes = require('./routes/api/notes');
 
